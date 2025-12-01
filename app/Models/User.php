e8a662d1da27b89b004816fe17373e7259e4a232
tin.keita <ipb23.t.paegle@vtdt.edu.lv>
2025-12-01 14:33:37 +0200

    izveidoti kontrolieri

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,40 +9,19 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    public function isAdmin()
+{
+    return $this->role === 'admin';
+}
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+public function isTeacher()
+{
+    return $this->role === 'teacher';
+}
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+public function isStudent()
+{
+    return $this->role === 'student';
+}
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
